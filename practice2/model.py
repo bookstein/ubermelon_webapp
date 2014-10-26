4b5091aa3f71cf5b54dfc45f940c90ff3b22a3ca
emilyboo <ehbookstein@gmail.com>
2014-10-26 14:39:27 -0700

    in get_all_melons, query the DB for top 30 results of melon table. iterate over fetchall rows to create melon instances with Melon class. NOTE: must create cursor inside get_all_melons using connect function.

--- practice2/model.py
+++ practice2/model.py
@@ -3,6 +3,20 @@ import sqlite3
 # DB = None
 # CONN = None
 
+class Melon(object):
+	"""Creates melon objects to structure and name data from DB """
+	def __init__(self, id, melon_type, common_name, price, imgurl):
+		self.id = id
+		self.melon_type = melon_type
+		self.common_name = common_name
+		self.price = price
+		self.imgurl = imgurl
+
+	def price_str(self):
+		# this is a string representation of the objct's price
+		#formats the price of the melon
+		return "$%.2f" % (self.price)
+
 def connect_to_DB():
 	# global DB, CONN
 	CONN = sqlite3.connect("melons.db")
@@ -10,9 +24,17 @@ def connect_to_DB():
 	return DB
 
 def get_all_melons():
-	connect_to_DB()
-	query = """SELECT * from melons LIMIT 30"""
+	melon_list = []
+
+	DB = connect_to_DB() # gets return val of cursor from connect function
+	query = """SELECT id, melon_type, common_name, price, imgurl
+	FROM melons
+	LIMIT 30"""
 	DB.execute(query)
-	melons = DB.fetchall()
-	CONN.close()
-	return melons
+	melons = DB.fetchall() # returns a list of matching rows
+
+	for melon in melons: # loops through one row at a time
+		m = Melon(melon[0], melon[1], melon[2], melon[3], melon[4])
+		melon_list.append(m)
+
+	return melon_list #returns a list of Melon instances
